a30555efcf8ff7857bd498d47208fd74f944de2e
Aayush3115 <sapkotaaayush708@gmail.com>
2026-01-20 10:57:09 +0545

    Implement user, community, mood, post, like, and comment models with admin registration

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+class User(AbstractUser):
+        bio = models.TextField(blank=True)
+        avatar = models.URLField(blank=True)
+        preferred_moods = models.ManyToManyField(
+        'moods.Mood',
+        blank=True,
+        related_name='preferred_by_users'
+    )
+
+        def __str__(self):
+            return self.username
