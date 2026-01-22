8ba5309b98b50df0ca9d531b39a4f1357b50e5bd
Aayush3115 <sapkotaaayush708@gmail.com>
2026-01-22 20:02:03 +0545

    Initial API integration

--- backend/posts/views.py
+++ backend/posts/views.py
@@ -2,11 +2,12 @@ from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAuthenticated
 from .models import Post
 from .serializers import PostSerializer
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 class PostViewSet(ModelViewSet):
     queryset = Post.objects.all().order_by('-created_at')
     serializer_class = PostSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,3 +3,4 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+export default App;
