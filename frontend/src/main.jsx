7114ebbd7f0becca3e364fe1220f23dbf4ebd959
Nitesh Pokhrel <niteshpokhrel84@gmail.com>
2026-01-28 21:24:06 +0545

    Add Login page and Navbar component with styling; remove old Login component

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Login from './Components/Login/Login'
+import Login from './pages/Login'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
