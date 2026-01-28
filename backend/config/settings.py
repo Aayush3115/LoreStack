7249ea9dab99be7c00be4cda129b34a837f1f50a
Aayush3115 <sapkotaaayush708@gmail.com>
2026-01-28 23:00:35 +0545

    fixed login and registration

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -169,3 +169,5 @@ AUTH_USER_MODEL = 'accounts.User'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+APPEND_SLASH = False
