cabdb110aa27f3348cdec4beac08769b95abce06
Aayush3115 <sapkotaaayush708@gmail.com>
2026-02-14 21:16:39 +0545

    refactor: streamline permissions handling in CommunityViewSet and remove redundant permission classes

--- backend/communities/views.py
+++ backend/communities/views.py
@@ -1,35 +1,34 @@
-from django.shortcuts import render
 from rest_framework.viewsets import ModelViewSet
-from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly, AllowAny
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from .models import Community
 from .serializers import CommunitySerializer
-
-from rest_framework import permissions
+from rest_framework_simplejwt.authentication import JWTAuthentication
 
 class CommunityViewSet(ModelViewSet):
     queryset = Community.objects.all()
     serializer_class = CommunitySerializer
+    authentication_classes = [JWTAuthentication]
 
     def get_permissions(self):
+        """
+        Instantiates and returns the list of permissions that this view requires.
+        """
         if self.action in ['list', 'retrieve']:
-            return [permissions.AllowAny()]
-        if self.action in ['join', 'leave', 'create', 'update', 'partial_update', 'destroy']:
-            return [permissions.IsAuthenticated()]
-        return [permissions.IsAuthenticatedOrReadOnly()]
+            return [AllowAny()]
+        return [IsAuthenticated()]
 
     def perform_create(self, serializer):
         serializer.save(created_by=self.request.user)
 
-
-    @action(detail=True, methods=['post'], permission_classes=[IsAuthenticatedOrReadOnly])
+    @action(detail=True, methods=['post'])
     def join(self, request, pk=None):
         community = self.get_object()
         community.members.add(request.user)
         return Response({'status': 'joined'})
 
-    @action(detail=True, methods=['post'], permission_classes=[IsAuthenticatedOrReadOnly])
+    @action(detail=True, methods=['post'])
     def leave(self, request, pk=None):
         community = self.get_object()
         community.members.remove(request.user)
