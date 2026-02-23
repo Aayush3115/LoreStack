48b23d92f1c8f80568a391a04fecdc89a44eec90
Aayush3115 <sapkotaaayush708@gmail.com>
2026-02-23 21:30:22 +0545

    added models and serializers for movie ratings and reviews

--- backend/movies/urls.py
+++ backend/movies/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import trending,movie_details     
+from .views import trending, movie_details, movie_rating
 
 urlpatterns = [
     path("trending/", trending, name="trending"),
     path("<int:movie_id>/", movie_details, name="movie_details"),
+    path("<int:movie_id>/rating/", movie_rating, name="movie_rating"),
 ]
