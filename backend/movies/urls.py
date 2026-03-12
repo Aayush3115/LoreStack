b85f06b1f7e97ecc920c85fc1d0dc6a8d3612d94
Aayush3115 <sapkotaaayush708@gmail.com>
2026-03-12 07:55:42 +0545

    tv series and anime details fetching from tmbd and anilist

--- backend/movies/urls.py
+++ backend/movies/urls.py
@@ -1,5 +1,14 @@
 from django.urls import path
-from .views import trending_movies, trending_tv, trending_anime, movie_details, tv_details, movie_rating, universal_search, movie_activity, user_watchlist, movie_recommendations
+from .views import (
+    trending_movies, trending_tv, trending_anime, 
+    movie_details, tv_details, 
+    movie_rating, tv_rating, anime_rating,
+    universal_search, 
+    movie_activity, tv_activity, anime_activity,
+    user_watchlist, 
+    movie_recommendations, tv_recommendations,
+    anime_details
+)
 
 urlpatterns = [
     path("trending-movies/", trending_movies, name="trending_movies"),
@@ -9,7 +18,13 @@ urlpatterns = [
     path("user-watchlist/", user_watchlist, name="user_watchlist"),
     path("<int:movie_id>/", movie_details, name="movie_details"),
     path("tv/<int:tv_id>/", tv_details, name="tv_details"),
+    path("anime/<int:anime_id>/", anime_details, name="anime_details"),
     path("<int:movie_id>/rating/", movie_rating, name="movie_rating"),
+    path("tv/<int:tv_id>/rating/", tv_rating, name="tv_rating"),
+    path("anime/<int:anime_id>/rating/", anime_rating, name="anime_rating"),
     path("<int:movie_id>/activity/", movie_activity, name="movie_activity"),
+    path("tv/<int:tv_id>/activity/", tv_activity, name="tv_activity"),
+    path("anime/<int:anime_id>/activity/", anime_activity, name="anime_activity"),
     path("<int:movie_id>/recommendations/", movie_recommendations, name="movie_recommendations"),
+    path("tv/<int:tv_id>/recommendations/", tv_recommendations, name="tv_recommendations"),
 ]
